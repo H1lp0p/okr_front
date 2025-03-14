15476f12df021fdffe57152a70189d3c0e58d15d
NickolayRogov <kolya190509@mail.ru>
2025-03-14 07:28:40 +0700

    Сделал логин и регистрацию. Связал с backendom. TODO: разобраться с политикой CORS

--- okr_front/src/components/register-form/register-form.tsx
+++ okr_front/src/components/register-form/register-form.tsx
@@ -1,35 +1,57 @@
-import UserModel from '../../models/UserModel';
-import Input from '../inputs/input';
-import Button from '../buttons/button';
-import 'bootstrap';
+import Input from '../inputs/CustomInput';
+import CustomBtn from '../buttons/CustomBtn';
+import BaseProps from '../Base/BasePropsInterface';
+import CustomForm from '../form/CustomForm';
+import "./Register-form.css";
+import { useState } from 'react';
 
-interface registerProps{
+interface registerProps extends BaseProps{
+    registration: (email: string, name:string, bithdate: string,  password: string) => void;
 }
 
 function Registration(props: registerProps){
+    const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
+    const [date, setDate] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const handleSubmin = (event: React.FormEvent<HTMLFormElement>) =>{
+            event.preventDefault();
+            try{
+                props.registration(email, name, date, password);
+            }catch (e: any){
+                setError(e);
+            }
+        };
     return (
-        <form id="register-form">
+        <CustomForm id="register-form" className='register-cont container-sm border-right border-bottom custom-rounded custom-shadow d-block p-5' divClass='container d-flex justify-content-center align-items-center vh-100'
+        onSubmin={handleSubmin} error={error}>
             <Input
             placehodler={"Адрес электронной почты"}
             inputType={"email"}
-            classAttr={"form-control auth-input"}/>
+            classAttr={"form-control auth-input"}
+            onChange={(e) => setEmail(e.target.value)}/>
             <Input
             placehodler={"Фамилия имя отчество"}
             inputType={"text"}
-            classAttr={"form-control auth-input"}/>
+            classAttr={"form-control auth-input"}
+            onChange={(e) => setName(e.target.value)}/>
             <Input
             placehodler={"Дата рождения"}
-            inputType={"date"}
-            classAttr={"form-control auth-input"}/>
+            inputType={"Date"}
+            classAttr={"form-control auth-input"}
+            onChange={(e) => setDate(e.target.value)}/>
             <Input
             placehodler={"Пароль"}
             inputType={"password"}
-            classAttr={"form-control auth-input"}/>
-            <Button
+            classAttr={"form-control auth-input"}
+            onChange={(e) => setPassword(e.target.value)}/>
+            <CustomBtn
             buttonType={"submit"}
-            classAttr={"btn btn-secondary btn-auth"}
-            value={"Зарегистрироваться"}/>
-        </form>
+            classAttr={"btn btn-secondary btn-auth registr-btn"}
+            value={"Зарегистрироваться"}
+            className={"registr-btn-cont"}/>
+        </CustomForm>
     );
 }
 export default Registration;
